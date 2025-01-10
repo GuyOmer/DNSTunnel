17a840dfb82f35a1ccaa6b8a2c890e892586e2c6
yaaraverner <yaara@vernermail.com>
2025-01-10 13:28:46 +0200

    use base _handle_incoming_ingress_message in socks_server.py

--- dns_tunnel/socks_handlers/socks_server.py
+++ dns_tunnel/socks_handlers/socks_server.py
@@ -39,7 +39,7 @@ class SOCKS5HandshakeState(enum.Enum):
 class ProxyServerHandler(BaseHandler):
     def __init__(self, logger: logging.Logger):
         super().__init__(logger)
-        self._session_id_to_destination: dict[int, TCPClientSocket] = {}
+        self._session_id_to_destination: dict[int, TCPClientSocket | None] = {}
         self._session_id_to_socks5_handshake_state: dict[int, SOCKS5HandshakeState] = {}
 
     def address(self):
@@ -101,101 +101,86 @@ class ProxyServerHandler(BaseHandler):
 
     def remove_edge_by_session_id(self, session_id: int) -> None:
         logger.info(f"Closing session {session_id}")
-        del self._session_id_to_destination[session_id]
+        self._session_id_to_destination[session_id] = None
 
     def _handle_incoming_ingress_message(self, ingress: ProxySocket, msg: DNSPacket):
-        logger.debug(f"Handling incoming message for session {msg.header.session_id}")
-
-        if msg.header.message_type == MessageType.ACK_MESSAGE:
-            ingress.ack_message(msg.header.session_id, msg.header.sequence_number)
-            logger.debug(f"ACK message for session {msg.header.session_id}, sequence {msg.header.sequence_number}")
+        if msg.header.message_type == MessageType.NORMAL_MESSAGE and msg.header.session_id not in self._session_id_to_destination:
+            logger.debug(f"Handling incoming message for session {msg.header.session_id}")
+            self._handle_socks5_handshake(ingress, msg)
             return
 
-        if msg.header.message_type == MessageType.CLOSE_SESSION:
-            self.remove_edge_by_session_id(msg.header.session_id)
-            return
+        super()._handle_incoming_ingress_message(ingress, msg)
 
-        # Handle the message
-        if msg.header.session_id not in self._session_id_to_destination:
-            # Session is still in SOCKS5 handshake phase
-            logger.debug(f"Session {msg.header.session_id} in SOCKS5 handshake phase")
+    def _handle_socks5_handshake(self, ingress: ProxySocket, msg: DNSPacket):
+        # Session is still in SOCKS5 handshake phase
+        logger.debug(f"Session {msg.header.session_id} in SOCKS5 handshake phase")
 
-            # Check if start of handshake
-            if msg.header.session_id not in self._session_id_to_socks5_handshake_state:
-                self._session_id_to_socks5_handshake_state[msg.header.session_id] = (
-                    SOCKS5HandshakeState.WAITING_FOR_CONNECT_REQUEST
-                )
-                greeting_msg = SOCKS5Greeting.from_bytes(msg.payload)
-                if SOCKS5AuthMethod.NO_AUTH not in greeting_msg.auth_methods:
-                    logger.error("Only no-auth is supported")
-                    raise ValueError("Only no-auth is supported")
+        # Check if start of handshake
+        if msg.header.session_id not in self._session_id_to_socks5_handshake_state:
+            self._session_id_to_socks5_handshake_state[msg.header.session_id] = (
+                SOCKS5HandshakeState.WAITING_FOR_CONNECT_REQUEST
+            )
+            greeting_msg = SOCKS5Greeting.from_bytes(msg.payload)
+            if SOCKS5AuthMethod.NO_AUTH not in greeting_msg.auth_methods:
+                logger.error("Only no-auth is supported")
+                raise ValueError("Only no-auth is supported")
 
-                ingress.add_to_write_queue(
-                    SOCKS5GreetingResponse(SOCKS5AuthMethod.NO_AUTH).to_bytes(),
-                    msg.header.session_id,
-                )
-                logger.debug(f"Sent SOCKS5 greeting response for session {msg.header.session_id}")
-            # Handshake already in progress
-            else:
-                match self._session_id_to_socks5_handshake_state[msg.header.session_id]:
-                    case SOCKS5HandshakeState.WAITING_FOR_CONNECT_REQUEST:
-                        command_msg = SOCKS5DNSConnectRequest.from_bytes(msg.payload)
-                        if command_msg.command != SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION:
-                            logger.error(f"Only {SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION} is supported")
-                            raise ValueError(
-                                f"Only {SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION} is supported"
-                            )
-
-                        dest_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                        dest_sock.setblocking(False)
-
-                        try:
-                            try:
-                                dest_sock.connect((command_msg.address, command_msg.port))
-                            except BlockingIOError as e:
-                                _, connected, __ = select.select([], [dest_sock], [], 0.4)
-                                if not connected:
-                                    raise ConnectionRefusedError("Connection refused") from e
-                        except (socket.gaierror, ConnectionRefusedError, BlockingIOError) as e:
-                            logger.error(f"Failed to connect to {command_msg.address}:{command_msg.port} with {e}")
-                            ingress.add_to_write_queue(
-                                SOCKS5DNSConnectResponse(
-                                    SOCKS5ConnectRequestStatus.HOST_UNREACHABLE,
-                                    command_msg.address,
-                                    command_msg.port,
-                                ).to_bytes(),
-                                msg.header.session_id,
-                            )
-                            logger.error(
-                                f"Host {command_msg.address}:{command_msg.port} not found for session {msg.header.session_id}"
-                            )
-                            return
-
-                        self._session_id_to_destination[msg.header.session_id] = TCPClientSocket(
-                            dest_sock, msg.header.session_id
+            ingress.add_to_write_queue(
+                SOCKS5GreetingResponse(SOCKS5AuthMethod.NO_AUTH).to_bytes(),
+                msg.header.session_id,
+            )
+            logger.debug(f"Sent SOCKS5 greeting response for session {msg.header.session_id}")
+        # Handshake already in progress
+        else:
+            match self._session_id_to_socks5_handshake_state[msg.header.session_id]:
+                case SOCKS5HandshakeState.WAITING_FOR_CONNECT_REQUEST:
+                    command_msg = SOCKS5DNSConnectRequest.from_bytes(msg.payload)
+                    if command_msg.command != SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION:
+                        logger.error(f"Only {SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION} is supported")
+                        raise ValueError(
+                            f"Only {SOCKS5CommandCode.ESTABLISH_A_TCP_IP_STREAM_CONNECTION} is supported"
                         )
+
+                    dest_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    dest_sock.setblocking(False)
+
+                    try:
+                        try:
+                            dest_sock.connect((command_msg.address, command_msg.port))
+                        except BlockingIOError as e:
+                            _, connected, __ = select.select([], [dest_sock], [], 0.4)
+                            if not connected:
+                                raise ConnectionRefusedError("Connection refused") from e
+                    except (socket.gaierror, ConnectionRefusedError, BlockingIOError) as e:
+                        logger.error(f"Failed to connect to {command_msg.address}:{command_msg.port} with {e}")
                         ingress.add_to_write_queue(
                             SOCKS5DNSConnectResponse(
-                                SOCKS5ConnectRequestStatus.GRANTED, command_msg.address, command_msg.port
+                                SOCKS5ConnectRequestStatus.HOST_UNREACHABLE,
+                                command_msg.address,
+                                command_msg.port,
                             ).to_bytes(),
                             msg.header.session_id,
                         )
-                        logger.info(
-                            f"Established TCP connection to {command_msg.address}:{command_msg.port} for session {msg.header.session_id}"
+                        logger.error(
+                            f"Host {command_msg.address}:{command_msg.port} not found for session {msg.header.session_id}"
                         )
-                    case _:
-                        logger.error("Invalid state")
-                        raise ValueError("Invalid state")
+                        return
 
-        else:
-            # proxy tunnel already setup, just forward messages
-            destination = self._session_id_to_destination[msg.header.session_id]
-            if not destination:
-                logger.debug("Message for a deleted destination")
-                return
-
-            destination.add_to_write_queue(msg.payload)
-            logger.debug(f"Forwarded message to destination for session {msg.header.session_id}")
+                    self._session_id_to_destination[msg.header.session_id] = TCPClientSocket(
+                        dest_sock, msg.header.session_id
+                    )
+                    ingress.add_to_write_queue(
+                        SOCKS5DNSConnectResponse(
+                            SOCKS5ConnectRequestStatus.GRANTED, command_msg.address, command_msg.port
+                        ).to_bytes(),
+                        msg.header.session_id,
+                    )
+                    logger.info(
+                        f"Established TCP connection to {command_msg.address}:{command_msg.port} for session {msg.header.session_id}"
+                    )
+                case _:
+                    logger.error("Invalid state")
+                    raise ValueError("Invalid state")
 
 
 if __name__ == "__main__":
